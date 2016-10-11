42d19736d7c24202b1c8a3bc34c9596e847c6ea8
曾诗 <zengshi@cengshidemacbook.local>
2016-10-11 09:29:43 +0800

    Initial Commit

---
+++ CodeShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeShare
+//
+//  Created by 曾诗 on 16/10/11.
+//  Copyright © 2016年 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
